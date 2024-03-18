fef517b370383d7b11b9738363f48cdab9625228
sakhi baksh <161965468+sakhibux@users.noreply.github.com>
2024-03-18 22:25:40 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,38 @@
+import inqurer from "inquirer";
+
+const answer = await inqurer.prompt([
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  { message: "Enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multipliction", "Division"],
+  },
+]);
+
+// condition statment
+if (answer.operator === "Addition") {
+    console.log( answer.firstNumber + answer.secondNumber);
+}
+
+else if(answer.operator === "Subtraction") {
+    console.log( answer.firstNumber - answer.secondNumber);
+}
+
+else if(answer.operator === "Multipliction") {
+    console.log( answer.firstNumber * answer.secondNumber);
+
+}
+else if(answer.operator === "Division") {
+    console.log( answer.firstNumber / answer.secondNumber);
+
+}
+else {
+    console.log("please select valid operator")
+}
+
+
+
+
+    
